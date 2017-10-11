f7005b9a54b18e48fdd81789518393dfe0af39ea
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-10 19:02:09 -0500

    next player

--- src/index.js
+++ src/index.js
@@ -21,8 +21,12 @@ class Board extends React.Component {
 
     handleClick(i) {
     const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({
+        squares: squares,
+        xIsNext : !this.state.xIsNext,
+    });
   }
 
   renderSquare(i){
@@ -35,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
